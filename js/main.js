050f5c548ceb222edfb3ae5b6b44cfc50dfda0c0
juan <mtzn_juandaniel@yahoo.com.mx>
2020-05-11 04:05:32 -0600

    Showing menu in mobile

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var menu = document.getElementById('header__menu'),
+    modal = document.getElementById('modal'),
+    hamburger = document.getElementById('header__hamburger');
+iconMenu = document.querySelector('.header__hamburger img');
+
+if (window.screen.width <= 375) {
+    menu.classList.toggle('hidden');
+}
+
+hamburger.addEventListener('click', function () {
+    if (modal.classList.contains('hidden')) {
+        iconMenu.src = '../img/icon-close.svg';
+    } else {
+        iconMenu.src = './img/icon-hamburger.svg';
+    }
+    modal.classList.toggle('hidden');
+    menu.classList.toggle('hidden');
+});
